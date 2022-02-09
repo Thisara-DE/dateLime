0457905ce132eca6a06473fbb562c4f56372ca3c
Cha Vue <chavue09@gmail.com>
2022-02-09 00:40:51 -0600

    Updated api fetch and js

--- assets/js/recipelist.js
+++ assets/js/recipelist.js
@@ -1,30 +1,42 @@
-var usersContainer = document.getElementById('users');
-var fetchButton = document.getElementById('fetch-button');
+var recipeContainer = document.getElementById('recipe');
 
-function getApi() {
-  var requestUrl = 'https://api.github.com/users?per_page=5';
 
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      //Using console.log to examine the data
-      console.log(data);
-      for (var i = 0; i < data.length; i++) {
-        //Creating a h3 element and a p element
-        var userName = document.createElement('h3');
-        var userUrl = document.createElement('p');
-
-        //Setting the text of the h3 element and p element.
-        userName.textContent = data[i].login;
-        userUrl.textContent = data[i].url;
-
-        //Appending the dynamically generated html to the div associated with the id="users"
-        //Append will attach the element as the bottom most child.
-        usersContainer.append(userName);
-        usersContainer.append(userUrl);
-      }
+function getRecipes() {
+fetch("https://tasty.p.rapidapi.com/recipes/list?from=0&size=4&tags=under_30_minutes", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "tasty.p.rapidapi.com",
+		"x-rapidapi-key": "c932fbbb24mshb43abde3fc25cffp11cacajsnae76ac16d611"
+	}
+})
+.then(function(response) {
+	console.log(response);
+    response.json().then(function(data) {
+        console.log(data);
+		
     });
-}
-fetchButton.addEventListener('click', getApi);
+})
+// .then(function (data) {
+    
+//     for (var i = 0; i < data.length; i++) {
+//         var recipeName = document.createElement("<p class='title is-4'>recipe</p>")
+
+//         recipeName.textContent = data[i].name;
+        
+
+//         recipeContainer.append(recipe);
+//     }
+// });
+};
+
+onload = (event) => {
+    getRecipes();
+  };
+
+
+// Store data
+// var someData = 'The data that I want to store for later.';
+// localStorage.setItem('myDataKey', someData);
+
+// Get data
+// var data = localStorage.getItem('myDataKey');
