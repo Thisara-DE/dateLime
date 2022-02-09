df8deb2397695d4e0bea1590c7580ce5ed6e6ed4
rharris529 <jordandine@gmail.com>
2022-02-09 15:32:02 -0600

    null

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,4 +1,17 @@
 var queryString = document.location.search;
+
+// Genre button choices
+var action = document.getElementById("action");
+var drama = document.getElementById("drama");
+var comedy = document.getElementById("comedy");
+var horror = document.getElementById("horror");
+
+// Rating button choices 
+var g = document.getElementById("G");
+var pg = document.getElementById("PG");
+var pg13 = document.getElementById("PG-13");
+var r = document.getElementById("R");
+
 function parseQuery(queryString) {
     var query = {};
     var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
@@ -7,6 +20,12 @@ function parseQuery(queryString) {
         query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
     }
     return query;
+
 }
+
+if (action === "clicked") {
+    
+}
+
 
 console.log("query params from url",parseQuery(queryString));
