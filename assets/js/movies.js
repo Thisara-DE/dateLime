4b6aa0a76348412fe1d92f13d0e229d738b177a4
rharris529 <jordandine@gmail.com>
2022-02-10 17:52:46 -0600

    linked to movielist.html

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,38 +1,22 @@
-var goBtn = document.querySelector("#goBtn");
+var goBtn = document.querySelector("#save-btn");
 
-// Genre id tags
-var action = document.getElementById("28");
-var drama = document.getElementById("18");
-var comedy = document.getElementById("35");
-var horror = document.getElementById("27");
-
-// Rating id tags
-var g = document.getElementById("1");
-var pg = document.getElementById("2");
-var pg13 = document.getElementById("3");
-var r = document.getElementById("4");
-
-var saveID = function() {
-    var saved = document.querySelector("input:checked").value;
-    localStorage.setItem("saved", saved);
-
-    console.log(saved);
-};
+var genreClass = document.getElementsByClassName("genre");
 
 // link to movielist page
-var goToNewPage = function() {
+var goToNewPage = function(certId, genreId) {
 
-    var url = url("http://127.0.0.1:5501/movielist.html?info=3,14&genre=");
+    var url = "./movielist.html?info=" + certId + "," + genreId
 
-    // turn goBtn into a hyperlink to movielist page
-    var link = document.createElement("a");
-    link.setAttribute("href", url);
-    URLSearchParams.append("Action", action);
-    link.setAttribute("target", "_blank");
-    
-
-    
+    window.location.href = url;
 };
 
-saveID();
-goBtn.addEventListener("click", goToNewPage());
+goBtn.addEventListener("click", function(){
+
+    var certInput = document.querySelector(".rating:checked").id;
+
+    var genreInput = document.querySelector(".genre:checked").id;
+
+    console.log("Let's go Clicked", certInput, genreInput);
+
+    goToNewPage(certInput, genreInput)
+});
