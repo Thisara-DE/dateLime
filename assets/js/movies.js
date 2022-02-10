1a7d0419a3602dd7ecf1b7c0ade657039cfca423
rharris529 <jordandine@gmail.com>
2022-02-10 12:05:18 -0600

    need to save ID to localStorage

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,5 +1,4 @@
 var queryString = document.location.search;
-console.log(queryString);
 var userClickedGenre = '';
 var goBtn = document.querySelector("#goBtn");
 var genre = document.getElementsByClassName("genre");
@@ -15,27 +14,43 @@ var id = {
     "Horror": 27,
 };
 
-// parse genre and rating ids into query params
-function parseQuery(queryString) {
-    var query = id;
-    var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
-    for (var i = 0; i < genre.length; i++) {
-        var pair = pairs[i].split('=');
-        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+var saveID = function() {
+
+    var action = document.getElementsById("28");
+    var drama = document.getElementById("18");
+    var comedy = document.getElementById("35");
+    var horror = document.getElementById("27");
+    
+    if (action === "click") {
+        localStorage.setItem(action.textcontent);
+        console.log(action);
     }
-    console.log(query);
-    return query;
+    
+
 };
+
+// parse genre and rating ids into query params
+// function parseQuery(queryString) {
+//     var query = id;
+//     var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+//     for (var i = 0; i < genre.length; i++) {
+//         var pair = pairs[i].split('=');
+//         query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+//     }
+//     console.log(query);
+//     return query;
+// };
 
 // link to movielist page
 var goToNewPage = function(goBtn) {
 
     // turn goBtn into a hyperlink to movielist page
     var link = document.createElement("a");
-    link.setAttribute("href", "http://127.0.0.1:5501/movielist.html?info=3,14");
+    link.setAttribute("href", "./movielist.html?info=3,14");
     link.setAttribute("target", "_blank");
     goBtn.appendChild(link);
 }
 
 parseQuery();
 goBtn.addEventListener("click", goToNewPage());
+saveID();
